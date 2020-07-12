f5db7d85d7e55c334adc945010130d592368275e
Hodgiecode <55938752+Hodgiecode@users.noreply.github.com>
2020-07-12 17:46:35 +0500

    Update stt.py

--- stt.py
+++ stt.py
@@ -12,7 +12,6 @@ app = Flask(__name__)
 
 @app.route('/myfunc',methods=['GET', 'POST'])
 def myfunc():
-    ### считали строку
     arg1= request.args.get('url', default = '1', type = str)
     arg2 = request.args.get('id',default = '*', type = str)
 
@@ -30,16 +29,16 @@ def myfunc():
     ff.run()
 
     ## речь в звук
-    access_token="WNUZ5JT2LMFVIQB4QXMWCX4LH6XUU34Z"
+    access_token="WIT-TOKEN"
     client = Wit(access_token)
     resp = None
     with open('file_out.wav', 'rb') as f:
         resp = client.speech(f, None, {'Content-Type': 'audio/wav'})
 
-    url_= 'https://api.telegram.org/bot1053711456:AAG4r3cVeVrTm1DYY_KIz950dLXLjhyy5d0/sendMessage?chat_id='+arg2+'&text='+str(resp['text'])
+    url_= 'https://api.telegram.org/botTOKEN/sendMessage?chat_id='+arg2+'&text='+str(resp['text'])
     requests.post(url_)
-    os.remove("file_in.oga")
-    os.remove("file_out.wav")
+    #os.remove("file_in.oga")
+    #os.remove("file_out.wav")
     return url_
 
 if __name__ == '__main__':
