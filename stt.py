5e3a51b80217377b2a1c7000e00335ebed5cc409
Hodgiecode <55938752+Hodgiecode@users.noreply.github.com>
2020-07-12 15:48:00 +0500

    Add files via upload

--- stt.py
+++ stt.py
@@ -4,53 +4,44 @@ from wit import Wit
 import os
 import requests
 from urllib.request import urlopen
+from flask import request
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/myfunc',methods=['GET', 'POST'])
 def myfunc():
     ### считали строку
-    cid=""
+    arg1= request.args.get('url', default = '1', type = str)
+    arg2 = request.args.get('id',default = '*', type = str)
 
-    data = urlopen("https://drive.google.com/uc?export=download&id=1Eker9jD9iC4QPEfBbV0SZ1hSDq1ZsdXq")
-	##В файле 2 строки разделенных '\n' url и chatid
+    data = urlopen(arg1).read()
 
-    i=0
-    for line in data:
-        if i==0:
-            inf=line.decode("utf-8").rstrip()
-        if i==1:
-            cid=line.decode("utf-8").rstrip()
-
-        i=i+1
+    with open('file_in.oga', 'wb') as f:
+        f.write(data)
 
     ## конвертируем
     ff = ffmpy.FFmpeg(
-        inputs={inf: None},
-        outputs={'file_super.wav': None}
+        inputs={'file_in.oga': None},
+        outputs={'file_out.wav': None}
     )
 
     ff.run()
 
-    ### речь в звук
-    access_token="XXXXXX"
+    ## речь в звук
+    access_token="WNUZ5JT2LMFVIQB4QXMWCX4LH6XUU34Z"
     client = Wit(access_token)
     resp = None
-    with open('file_super.wav', 'rb') as f:
+    with open('file_out.wav', 'rb') as f:
         resp = client.speech(f, None, {'Content-Type': 'audio/wav'})
 
-    url_= 'https://api.telegram.org/botID/sendMessage?chat_id='+cid+'&text='+str(resp['_text'])
+    url_= 'https://api.telegram.org/bot1053711456:AAG4r3cVeVrTm1DYY_KIz950dLXLjhyy5d0/sendMessage?chat_id='+arg2+'&text='+str(resp['text'])
     requests.post(url_)
-    os.remove("file_super.wav")
+    os.remove("file_in.oga")
+    os.remove("file_out.wav")
     return url_
-
-
 
 if __name__ == '__main__':
     app.run()
-
-
-
 
